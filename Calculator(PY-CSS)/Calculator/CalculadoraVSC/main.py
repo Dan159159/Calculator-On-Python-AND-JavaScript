70aec347ce4aa6130ce49682459651c83f5dffe7
Dan159159 <113427338+Dan159159@users.noreply.github.com>
2023-03-13 16:37:25 +0100

    ISSUES: the method rework and delete the before variables and operations

--- Calculator(PY-CSS)/Calculator/CalculadoraVSC/main.py
+++ Calculator(PY-CSS)/Calculator/CalculadoraVSC/main.py
@@ -5,54 +5,86 @@ app = Flask(__name__)
 def home():
     result = 0.0
     return render_template('index.html', result=result)
-@app.route('/press_button', methods=['POST','GET'])
+@app.route('/Calculator-BY:Dan', methods=['POST','GET'])
 def index():
+    result = 0.0
+    operation = None
+    result = float(request.form.get('result', 0))
+    num2=0.0
+
     if request.method == 'POST':
-        operation = None
         button_value = request.form['button']
         result = float(request.form.get('result', 0))
-        num1 = result
+        num1=float(request.form.get('result',0))
+        print(f"Primer resultado : {result}")
+        print(f"Variable num1 : {num1}")
+        print(f"")
         if button_value == 'C':
             result = 0.0
+            num1=result
             operation = None
         elif button_value == 'borrar':
-            operation = None
+            num1=result
             result = request.form.get('result',0)
             result = result[:-1]  # eliminar el último carácter
-            if result == '':
-                result = 0.0
+            operation = None
         elif button_value == 'suma':
-            operation = 1
-            num1 = result
-            result = float(request.form.get("",0))
+            num1=result
             result = 0.0  # reiniciar result
+            operation = 'plus'
+            print(f"resultado : {result}")
+            print(f"Variable num1 : {num1}")
+            print(f"Variable num2 : {num2}")
+            print(f"Operation : {operation}")
+            if button_value == 'igual':
+                num2 = float(request.form.get('result', 0))
+                print(f"resultado igual : {result}")
+                print(f"Variable num1 igual : {num1}")
+                print(f"Variable num2 igual : {num2}")
+                print(f"Operation igual : {operation}")
+                match operation:
+                    case 'suma':
+                        result = num1 + num2
+                    case 'minus':
+                        result = num1 - num2
+                    case 'multi':
+                        result = num1 * num2
+                    case 'div':
+                        result = num1 / num2
         elif button_value == 'minus':
-            operation = 2
+            num1=result
+            operation = 'minus'
             num1 = result
             result = float(request.form.get("",0))
 
         elif button_value == 'multi':
-            operation = 3
+            operation = 'multi'
             num1 = result
             result = float(request.form.get("",0))
 
         elif button_value == 'dividir':
-            operation = 4
-            num1 = result
+            operation = 'div'
+            num1 =float(request.form.get('result'))
             result = float(request.form.get("",0))
         elif button_value == 'igual':
-            num2 = result
+            num2 = float(request.form.get('result', 0))
+            print(f"resultado igual : {result}")
+            print(f"Variable num1 igual : {num1}")
+            print(f"Variable num2 igual : {num2}")
+            print(f"Operation igual : {operation}")
             match operation:
-                case 1:
-                    result = num1+num2
-                case 2:
+                case 'suma':
+                    result = num1 + num2
+
+                case 'minus':
                     result = num1-num2
-                case 3:
+                case 'multi':
                     result = num1*num2
-                case 4:
+                case 'div':
                     result = num1 / num2
+
         else:
-            result = result * 10 + float(button_value)
+            result = result *10+ float(button_value)
     else:
         result = 0
 
