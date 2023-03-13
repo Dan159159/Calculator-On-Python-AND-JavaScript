b06abeb30be7d3ab2f94a26f304107e895ee4e1b
Dan159159 <113427338+Dan159159@users.noreply.github.com>
2023-03-13 08:44:23 +0100

    Delete methods and comments

--- Calculator(PY-CSS)/Calculator/CalculadoraVSC/main.py
+++ Calculator(PY-CSS)/Calculator/CalculadoraVSC/main.py
@@ -1,74 +1,56 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-# Funciones para las operaciones
-def suma(num1, num2):
-    result = num1 + num2
-def resta(num1, num2):
-    result = num1 - num2
-    return result
-def dividir(num1, num2):
-    result = num1 / num2
-    return result
-def multi(num1, num2):
-    result = num1 * num2
-    return result
-# La primera pagina que nos saldra
 @app.route('/')
 def home():
-    result = 0
+    result = 0.0
     return render_template('index.html', result=result)
-# La pagina saldra cuando en el ocurra onaction='/press_button' en el html
-@app.route('/press_button', methods=['GET', 'POST'])
+@app.route('/press_button', methods=['POST','GET'])
 def index():
-    num1 = 0
-    num2 = 0
-    operation=''
-    result = 0
-    # Cuando post devolvamos en float el resultado con el value del boton
-    # Post the value of the button on the html
     if request.method == 'POST':
-        # Recoger el valor value='' del html// Get the value='' for the html
+        operation = None
         button_value = request.form['button']
-        # Mostrar el resultado en pantalla concadenando con el anterior resultado//Show the result concatenate the result
         result = float(request.form.get('result', 0))
         num1 = result
         if button_value == 'C':
-            result = 0
+            result = 0.0
+            operation = None
         elif button_value == 'borrar':
+            operation = None
             result = request.form.get('result',0)
             result = result[:-1]  # eliminar el último carácter
             if result == '':
                 result = 0.0
         elif button_value == 'suma':
-            operation = 'suma'
+            operation = 1
             num1 = result
             result = float(request.form.get("",0))
             result = 0.0  # reiniciar result
         elif button_value == 'minus':
-            operation = 'minus'
+            operation = 2
             num1 = result
             result = float(request.form.get("",0))
 
         elif button_value == 'multi':
-            operation = 'multi'
+            operation = 3
             num1 = result
             result = float(request.form.get("",0))
 
         elif button_value == 'dividir':
-            operation = 'dividir'
+            operation = 4
             num1 = result
             result = float(request.form.get("",0))
         elif button_value == 'igual':
             num2 = result
-            if operation == 'suma':
-                result = float(request.form.get('result', suma(num1, num2)))
-            elif operation == 'minus':
-                result = float(resta(num1, num2))
-            elif operation == 'multi':
-                result = multi(num1, num2)
-            elif operation == 'dividir':
-                result = dividir(num1, num2)
+            match operation:
+                case 1:
+                    result = num1+num2
+                case 2:
+                    result = num1-num2
+                case 3:
+                    result = num1*num2
+                case 4:
+                    result = num1 / num2
         else:
             result = result * 10 + float(button_value)
     else:
